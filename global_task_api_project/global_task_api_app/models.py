fb925570d094021faee58acf4084a473bdada2e2
Matias <matiasheimann@matiass-mbp.lan>
2024-02-06 21:06:48 -0300

    use unique for email

--- global_task_api_project/global_task_api_app/models.py
+++ global_task_api_project/global_task_api_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
     id = models.AutoField(primary_key=True)
 
 class Vehicle(models.Model):
